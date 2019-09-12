81108b5841afd7826d655279e0891c3cde17cdf3
OpKris <kristofferopdahl@mail.com>
2019-09-12 13:06:35 +0200

    added a math test

---
+++ src/test/java/no/kristiania/httpclient/HttpClientTest.java
@@ -0,0 +1,14 @@
+package no.kristiania.httpclient;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HttpClientTest {
+
+    @Test
+    void shouldwork (){
+        assertEquals(4, 2+2 );
+    }
+
+}// end httpClientTest
